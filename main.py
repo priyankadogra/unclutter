86d0e5aff2dfe956fa66b34621092cb424db6041
Ketaki Deo <ketaki@pulse.me>
2013-10-25 18:35:12 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from google.appengine.api import users
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
